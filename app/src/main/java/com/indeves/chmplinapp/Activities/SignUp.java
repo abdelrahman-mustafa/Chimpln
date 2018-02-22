deeb41dd218c88723bbca2cc50764fb9b2ad6209
boda <abdelrahmanmustaf00@gmail.com>
2018-02-22 15:12:40 +0200

    add registration and phone verification to the app not yet - we should use multiaple provider to link different auth to one account

--- app/src/main/java/com/indeves/chmplinapp/Activities/SignUp.java
+++ app/src/main/java/com/indeves/chmplinapp/Activities/SignUp.java
@@ -3,20 +3,44 @@ package com.indeves.chmplinapp.Activities;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.indeves.chmplinapp.R;
 
+import java.util.concurrent.TimeUnit;
+
 public class SignUp extends AppCompatActivity {
-    RadioButton radioButton,radioButton2,radioButton3;
+    RadioButton radioButton, radioButton2, radioButton3;
     RadioGroup radioGroup;
     Button createAccount;
+    private static String phoneNum;
+    EditText mail, phone, pass, confirmPass;
+    private static final String TAG = "EmailPassword";
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+  //  private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    private FirebaseAuth mAuth;
+    private String mVerificationId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,53 +51,112 @@ public class SignUp extends AppCompatActivity {
         radioButton = findViewById(R.id.signUp_radio_user_account);
         radioButton2 = findViewById(R.id.signUp_radio_pro_account);
         radioButton3 = findViewById(R.id.signUp_radio_stu_account);
+        mail = findViewById(R.id.signUp_email);
+        phone = findViewById(R.id.signUp_phone);
+        pass = findViewById(R.id.signUp_pass);
+        confirmPass = findViewById(R.id.signUp_confirm_pass);
+
 
         final ImageView splash = (ImageView) findViewById(R.id.splash);
         //splash.startAnimation(anim);
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.playTogether(
-                ObjectAnimator.ofFloat(splash,"translationY",-1000,0),
-                ObjectAnimator.ofFloat(splash,"alpha",0,1)
+                ObjectAnimator.ofFloat(splash, "translationY", -1000, 0),
+                ObjectAnimator.ofFloat(splash, "alpha", 0, 1)
         );
         animatorSet.setDuration(2000);
-       /* animatorSet.addListener(new AnimatorListenerAdapter(){
-            @Override public void onAnimationEnd(Animator animation) {
+        animatorSet.start();
 
-                AnimatorSet animatorSet2 = new AnimatorSet();
-                animatorSet2.playTogether(
-                        ObjectAnimator.ofFloat(splash,"scaleX", 1f, 0.5f, 1f),
-                        ObjectAnimator.ofFloat(splash,"scaleY", 1f, 0.5f, 1f)
-                );
-                animatorSet2.setInterpolator(new AccelerateInterpolator());
-                animatorSet2.setDuration(1000);
-                animatorSet2.start();
 
-            }
-        });
-       */ animatorSet.start();
+        // create account
+        mAuth = FirebaseAuth.getInstance();
+
+
         // get selected radio button from radioGroup
 
         // find the radiobutton by returned id
+
         createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d("mail", mail.getText().toString());
+                mAuth.createUserWithEmailAndPassword(mail.getText().toString().trim(), pass.getText().toString().trim())
+                        .addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(SignUp.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(SignUp.this, "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
 
 
-                if (radioButton.isChecked()) {
-                    Intent intent = new Intent(SignUp.this, SignUpConfirmCode.class);
-                    intent.putExtra("accountType","user");
-                    startActivity(intent);
-                }else if (radioButton2.isChecked()) {
-                    Intent intent = new Intent(SignUp.this, SignUpConfirmCode.class);
-                    intent.putExtra("accountType","pro");
-                    startActivity(intent);
+                                    final PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                                        @Override
+                                        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                                            Log.d("JEJE", "onVerificationCompleted:" + phoneAuthCredential);
+                                        }
 
-                }else {
-                    Intent intent = new Intent(SignUp.this, SignUpConfirmCode.class);
-                    intent.putExtra("accountType","stu");
-                    startActivity(intent);
-                }
+                                        @Override
+                                        public void onVerificationFailed(FirebaseException e) {
+                                            Log.w("JEJE", "onVerificationFailed", e);
+                                            if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                                                Log.d("JEJE", "INVALID REQUEST");
+                                            } else if (e instanceof FirebaseTooManyRequestsException) {
+                                                Log.d("JEJE", "Too many Request");
+                                            }
+                                        }
+                                        @Override
+                                        public void onCodeSent(String VerficationID, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                            super.onCodeSent(VerficationID, forceResendingToken);
+                                            Log.d("JEJE", "onCodeSent:" + VerficationID);
+                                            mResendToken = forceResendingToken;
+                                            phoneNum = phone.getText().toString();
+                                          // pass the verfication id and phomne number to check code
+                                            if (radioButton.isChecked()) {
+                                                Intent intent = new Intent(SignUp.this, SignUpConfirmCode.class);
+                                                intent.putExtra("accountType", "user");
+                                                intent.putExtra("Verfication ID",VerficationID);
+                                                startActivity(intent);
+                                            } else if (radioButton2.isChecked()) {
+                                                Intent intent = new Intent(SignUp.this, SignUpConfirmCode.class);
+                                                intent.putExtra("accountType", "pro");
+                                                intent.putExtra("Verfication ID",VerficationID);
+                                                startActivity(intent);
+
+                                            } else {
+                                                Intent intent = new Intent(SignUp.this, SignUpConfirmCode.class);
+                                                intent.putExtra("accountType", "stu");
+                                                intent.putExtra("Verfication ID",VerficationID);
+                                                startActivity(intent);
+                                            }
+                                        }
+                                    };
+                                    verifyPhone(phone.getText().toString().trim(),mCallBacks);
+
+
+                                }
+                            }
+                        });
+
+
             }
         });
+    }
+
+
+
+
+
+    public void verifyPhone(String phoneNumber, PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks){
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallback
     }
 }
--- app/src/main/java/com/indeves/chmplinapp/Activities/SignUpConfirmCode.java
+++ app/src/main/java/com/indeves/chmplinapp/Activities/SignUpConfirmCode.java
@@ -1,25 +1,49 @@
 package com.indeves.chmplinapp.Activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.indeves.chmplinapp.R;
 
 public class SignUpConfirmCode extends AppCompatActivity {
 
+    EditText codeEdit;
+    private static final String TAG = "PhoneAuthActivity";
     Button button;
-
+    private FirebaseAuth mAuth;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_confirm_code);
         button = findViewById(R.id.confirm_code);
+        mAuth = FirebaseAuth.getInstance();
+        codeEdit = findViewById(R.id.signup_verfication_code);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String verificationId =getIntent().getExtras().getString("Verfication ID");
+                String code = codeEdit.getText().toString().trim();
+                // [START verify_with_code]
+                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+                // [END verify_with_code]
+                signInWithPhoneAuthCredential(credential);
+
+
+
                 if (getIntent().getStringExtra("accountType").equals("user")) {
                     startActivity(new Intent(SignUpConfirmCode.this, UserProfileMain.class));
                 } else if (getIntent().getStringExtra("accountType").equals("pro")) {
@@ -29,5 +53,34 @@ public class SignUpConfirmCode extends AppCompatActivity {
                 }
             }
         });
+    }
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+
+                            FirebaseUser user = task.getResult().getUser();
+                            Log.d(TAG, user.getEmail().toString());
+
+                            // [START_EXCLUDE]
+                            // [END_EXCLUDE]
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                                // [START_EXCLUDE silent]
+                                // [END_EXCLUDE]
+                            }
+                            // [START_EXCLUDE silent]
+                            // Update UI
+                            // [END_EXCLUDE]
+                        }
+                    }
+                });
     }
 }
