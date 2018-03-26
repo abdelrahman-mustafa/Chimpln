448adfb432c5534893a4a232ce5990367bd70d71
khalidnaser2323 <khalid.naser2323@gmail.com>
2018-03-26 13:48:13 +0200

    [Technical]Lood lookups in search fragment.

--- app/src/main/java/com/indeves/chmplinapp/Fragments/UserProfilePhotographersTabSearchSelect.java
+++ app/src/main/java/com/indeves/chmplinapp/Fragments/UserProfilePhotographersTabSearchSelect.java
@@ -1,12 +1,10 @@
 package com.indeves.chmplinapp.Fragments;
 
 import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -27,34 +25,23 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-public class UserProfilePhotographersTabSearchSelect extends android.support.v4.app.Fragment implements View.OnClickListener, AdapterView.OnItemSelectedListener, View.OnTouchListener {
-    Spinner spinGender, spinCity, spinbirthPicker, spinEventType;
+public class UserProfilePhotographersTabSearchSelect extends android.support.v4.app.Fragment implements View.OnClickListener, AdapterView.OnItemSelectedListener {
+    Spinner spinGender, spinCity, eventTimesSpinner, spinEventType;
     Button search;
     List<CityLookUpModel> citiesList;
-    List<LookUpModel> countriesList;
     List<String> genderCategories;
     List<String> dayTimeCategories;
-    List<LookUpModel> eventTypesList = new ArrayList<>();
+    List<LookUpModel> eventTypesList, eventTimesList;
     ArrayAdapter<CityLookUpModel> citiesArrayAdapter;
-    ArrayAdapter<LookUpModel> eventTypeArrayAdapter;
+    ArrayAdapter<LookUpModel> eventTypeArrayAdapter, eventTimeArrayAdapter;
     TextView selectedDate;
     String selectedBirthDate;
     Calendar calendar;
     int year, month, day;
     String selectedGender;
     String selectedCity;
-    LookUpModel selectedEventType;
+    LookUpModel selectedEventType, selectedEventTime;
     LinearLayout linear;
-
-
-    public UserProfilePhotographersTabSearchSelect() {
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     private DatePickerDialog.OnDateSetListener myDateListener = new
             DatePickerDialog.OnDateSetListener() {
                 @Override
@@ -69,6 +56,14 @@ public class UserProfilePhotographersTabSearchSelect extends android.support.v4.
                 }
             };
 
+    public UserProfilePhotographersTabSearchSelect() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -79,13 +74,16 @@ public class UserProfilePhotographersTabSearchSelect extends android.support.v4.
         linear = rootView.findViewById(R.id.linear);
         day = calendar.get(Calendar.DAY_OF_MONTH);
         spinCity = rootView.findViewById(R.id.userProfile_phot_spinner_city);
+        spinCity.setOnItemSelectedListener(this);
 
         spinGender = rootView.findViewById(R.id.userProfile_phot_spinner_gender);
-        spinbirthPicker = rootView.findViewById(R.id.userProfile_phot_spinner_event_date);
+        spinGender.setOnItemSelectedListener(this);
+        //This spinner wil be used for event times
+        eventTimesSpinner = rootView.findViewById(R.id.userProfile_phot_spinner_event_date);
+        eventTimesSpinner.setOnItemSelectedListener(this);
         spinEventType = rootView.findViewById(R.id.userProfile_phot_spinner_event_type);
+        spinEventType.setOnItemSelectedListener(this);
         search = rootView.findViewById(R.id.userProfile_button_search);
-        spinbirthPicker.setOnItemSelectedListener(this);
-        spinbirthPicker.setOnTouchListener(this);
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -97,11 +95,10 @@ public class UserProfilePhotographersTabSearchSelect extends android.support.v4.
                     public void onProsResponse(ArrayList<ProUserModel> pros) {
                         //deal with pros
                         //    Log.v("ALlPros", pros.toString());
-
                         linear.setVisibility(View.GONE);
                         search.setVisibility(View.GONE);
                         for (int i = 0; i < pros.size(); i++) {
-                            if (pros.get(i).getCity() != null && pros.get(i).getCity().contains(spinCity.getSelectedItem().toString())|pros.get(i).getGender().contains(spinGender.getSelectedItem().toString())) {
+                            if (pros.get(i).getCity() != null && pros.get(i).getCity().contains(spinCity.getSelectedItem().toString()) | pros.get(i).getGender().contains(spinGender.getSelectedItem().toString())) {
                                 ArrayList<ProUserModel> customList = new ArrayList<>();
                                 customList.add(pros.get(i));
                                 UserProfilePhotographersTabSearchOutput output = new UserProfilePhotographersTabSearchOutput(customList);
@@ -121,31 +118,32 @@ public class UserProfilePhotographersTabSearchSelect extends android.support.v4.
         });
 
 
-
         genderCategories = new ArrayList<String>();
         genderCategories.add(getResources().getString(R.string.gender));
         genderCategories.add("Male");
         genderCategories.add("Female");
         // Creating adapter for spinner
-        ArrayAdapter<String> genderArrayAdapter = new ArrayAdapter<String>(getActivity().getBaseContext(), R.layout.spinner_item, genderCategories);
+        ArrayAdapter<String> genderArrayAdapter = new ArrayAdapter<String>(getActivity().getBaseContext(), android.R.layout.simple_spinner_item, genderCategories);
         // Drop down layout style - list view with radio button
         genderArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinGender.setAdapter(genderArrayAdapter);
         //------------------------------------------------
+        ReadData readData = new ReadData();
+
         citiesList = new ArrayList<CityLookUpModel>();
         citiesList.add(new CityLookUpModel(0, 0, "City"));
-        citiesArrayAdapter = new ArrayAdapter<CityLookUpModel>(getContext(), R.layout.spinner_item, citiesList);
+        citiesArrayAdapter = new ArrayAdapter<CityLookUpModel>(getContext(), android.R.layout.simple_spinner_item, citiesList);
         citiesArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinCity.setAdapter(citiesArrayAdapter);
-
-
-
-
-
-
-
+        readData.getCitiesLookUpsWithCountryId(-1, new ReadData.CityLookUpsListener() {
+            @Override
+            public void onLookUpsResponse(List<CityLookUpModel> returnedCitiesList) {
+                citiesList.addAll(returnedCitiesList);
+                citiesArrayAdapter.notifyDataSetChanged();
+            }
+        });
         //-------------------------------------------------
-
+        eventTypesList = new ArrayList<>();
         eventTypesList.add(0, new LookUpModel(0, getResources().getString(R.string.selectPackTy)));
         // Creating adapter for spinner
         eventTypeArrayAdapter = new ArrayAdapter<LookUpModel>(getContext(), android.R.layout.simple_spinner_item, eventTypesList);
@@ -153,12 +151,28 @@ public class UserProfilePhotographersTabSearchSelect extends android.support.v4.
         eventTypeArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinEventType.setAdapter(eventTypeArrayAdapter);
-        ReadData readData = new ReadData();
+
         readData.getLookupsByType("eventTypesLookups", new ReadData.LookUpsListener() {
             @Override
             public void onLookUpsResponse(List<LookUpModel> eventTypeLookups) {
                 Log.v("EventTypeLookupsArr", eventTypeLookups.toString());
-                eventTypeArrayAdapter.addAll(eventTypeLookups);
+                eventTypesList.addAll(eventTypeLookups);
+                eventTypeArrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+        //-------------------------------------------------
+        eventTimesList = new ArrayList<>();
+        eventTimesList.add(new LookUpModel(0, "Event time"));
+        eventTimeArrayAdapter = new ArrayAdapter<LookUpModel>(getContext(), android.R.layout.simple_spinner_item, eventTimesList);
+        eventTimeArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        eventTimesSpinner.setAdapter(eventTimeArrayAdapter);
+
+        readData.getLookupsByType("eventTimesLookups", new ReadData.LookUpsListener() {
+            @Override
+            public void onLookUpsResponse(List<LookUpModel> lookups) {
+                eventTimesList.addAll(lookups);
+                eventTimeArrayAdapter.notifyDataSetChanged();
             }
         });
 
@@ -190,15 +204,11 @@ public class UserProfilePhotographersTabSearchSelect extends android.support.v4.
 
     }
 
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        return false;
-    }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        if (parent.getId() == spinbirthPicker.getId()) {
-            //showDialog(999);
+        if (parent.getId() == eventTimesSpinner.getId()) {
+            selectedEventTime = eventTimesList.get(position);
         } else if (parent.getId() == spinGender.getId()) {
             selectedGender = genderCategories.get(position);
 
@@ -209,6 +219,7 @@ public class UserProfilePhotographersTabSearchSelect extends android.support.v4.
             selectedEventType = eventTypesList.get(position);
 
         }
+
 
 
 
